8bf300a1dd46591fa61e7d583f162bffaa929194
Airton Sales <airtonsales0@gmail.com>
2024-08-08 16:12:19 -0300

    feat: add Header component and banner

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,5 @@
 const About = () => {
-    return <h1>About page</h1>
+  return <h1 className="mb-5 bg-red-400 text-red-500">About page</h1>
 }
 
 export default About
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,35 @@
+import { SearchIcon } from "lucide-react"
 import { Button } from "./_components/ui/button"
+import Header from "./_components/ui/header"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
+
 const Home = () => {
-  return <Button>Button</Button>
+  return (
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Airton!</h2>
+        <p>Sexta, 2 de Fevereiro</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/Banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
