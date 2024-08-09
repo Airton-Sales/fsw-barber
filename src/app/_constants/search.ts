eb5fae3e84a481774250533579a5ecbf79372acd
Airton Sales <airtonsales0@gmail.com>
2024-08-09 13:50:15 -0300

    feat: add BookingItem component

---
+++ src/app/_constants/search.ts
@@ -0,0 +1,27 @@
+interface QuickSearchOption {
+  imageUrl: string
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    imageUrl: "/Cabelo.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/Barba.svg",
+    title: "Barba",
+  },
+  {
+    imageUrl: "/Acabamento.svg",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "/Sobrancelhas.svg",
+    title: "Sobrancelhas",
+  },
+  {
+    imageUrl: "/Hidratação.svg",
+    title: "Hidratação",
+  },
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,12 @@ import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar } from "./_components/ui/avatar"
-import { AvatarImage } from "@radix-ui/react-avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-Item"
+import { quickSearchOptions } from "./_constants/search"
+import BookingItem from "./_components/booking-item"
 
 const Home = async () => {
-  // chamar meu banco de dados
   const barbershops = await db.barbershop.findMany({})
   const poupularBarbershops = await db.barbershop.findMany({
     orderBy: {
@@ -39,50 +37,17 @@ const Home = async () => {
         {/* BUSCA RÁPIDA */}
 
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant={"secondary"}>
-            <Image src="/Cabelo.svg" alt="Cabelo" width={16} height={16} />
-            Cabelo
-          </Button>
-
-          <Button className="gap-2" variant={"secondary"}>
-            <Image src="/Barba.svg" alt="Barba" width={16} height={16} />
-            Barba
-          </Button>
-
-          <Button className="gap-2" variant={"secondary"}>
-            <Image
-              src="/Acabamento.svg"
-              alt="Acabamento"
-              width={16}
-              height={16}
-            />
-            Acabamento
-          </Button>
-
-          <Button className="gap-2" variant={"secondary"}>
-            <Image
-              src="/Sobrancelhas.svg"
-              alt="Sobrancelha"
-              width={16}
-              height={16}
-            />
-            Sobrancelha
-          </Button>
-
-          <Button className="gap-2" variant={"secondary"}>
-            <Image src="/Massagem.svg" alt="Massagem" width={16} height={16} />
-            Massagem
-          </Button>
-
-          <Button className="gap-2" variant={"secondary"}>
-            <Image
-              src="/Hidratação.svg"
-              alt="Hidratação"
-              width={16}
-              height={16}
-            />
-            Hidratação
-          </Button>
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-2" variant={"secondary"} key={option.title}>
+              <Image
+                src={option.imageUrl}
+                alt={option.title}
+                width={16}
+                height={16}
+              />
+              {option.title}
+            </Button>
+          ))}
         </div>
 
         {/* IMAGEM */}
@@ -96,30 +61,8 @@ const Home = async () => {
         </div>
 
         {/* AGENDAMENTO */}
-        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Agendamentos
-        </h2>
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            {/* ESQUERDA */}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Vintage Barber</p>
-              </div>
-            </div>
-            {/* DIREITA */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Fevereiro</p>
-              <p className="text-2xl font-bold">06</p>
-              <p className="text-sm">09:00</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
+
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
         </h2>
