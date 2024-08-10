d81939830f3063da862c6228f7eaa3ed1e4131ce
Airton Sales <airtonsales0@gmail.com>
2024-08-10 14:04:03 -0300

    feat: add PhoneItem component and Footer to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { Button } from "./_components/ui/button"
 import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
-import { Card, CardContent } from "./_components/ui/card"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-Item"
 import { quickSearchOptions } from "./_constants/search"
@@ -81,15 +80,6 @@ const Home = async () => {
           ))}
         </div>
       </div>
-      <footer>
-        <Card>
-          <CardContent className="px-6 py-5">
-            <p className="text-sm text-gray-400">
-              © 2023 Copyright <span className="font-bold">FSW Barber</span>
-            </p>
-          </CardContent>
-        </Card>
-      </footer>
     </div>
   )
 }
