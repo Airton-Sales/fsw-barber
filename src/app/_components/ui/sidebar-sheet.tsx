53a36904f34b4c4d13fe097fae03d9b6d5643b8c
Airton Sales <airtonsales0@gmail.com>
2024-08-11 15:20:26 -0300

    chore: add next auth setup

--- src/app/_components/ui/sidebar-sheet.tsx
+++ src/app/_components/ui/sidebar-sheet.tsx
@@ -2,9 +2,16 @@ import { Button } from "./button"
 import { CalendarIcon, HomeIcon, LogInIcon } from "lucide-react"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./sheet"
 import { quickSearchOptions } from "@/app/_constants/search"
-import { Avatar, AvatarImage } from "./avatar"
 import Link from "next/link"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./dialog"
 
 const SidebarSheet = () => {
   return (
@@ -13,7 +20,33 @@ const SidebarSheet = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>
+              <LogInIcon size="icon" />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça seu login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button className="gap-1 font-bold" variant={"outline"}>
+              <Image
+                alt="Fazer login com o Google"
+                src="/Google.svg"
+                width={20}
+                height={20}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* Avatar 
         <Avatar>
           <AvatarImage
             src="https://images.unsplash.com/photo-1543610892-0b1f7e6d8ac1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -24,7 +57,7 @@ const SidebarSheet = () => {
         <div>
           <p className="font-bold">Rafael Carvalho</p>
           <p className="text-xs">LXt4m@example.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
